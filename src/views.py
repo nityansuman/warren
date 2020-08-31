e040175e8da5a0ec3af99010f34ab277612756ad
nityansuman <nityan.suman@gmail.com>
2020-09-01 05:07:23 +0530

    Updated flask view

--- src/views.py
+++ src/views.py
@@ -1,4 +1,4 @@
-# Copyright 2020 The `Kumar Nityan Suman` (https://github.com/nityansuman/). All Rights Reserved.
+# Copyright 2020 `Kumar Nityan Suman` (https://github.com/nityansuman/). All Rights Reserved.
 #
 #                     GNU GENERAL PUBLIC LICENSE
 #                        Version 3, 29 June 2007
@@ -10,77 +10,57 @@
 
 # Import packages
 import os
-import csv
+from src import app
 import pandas as pd
-import numpy as np
-from datetime import datetime
-from fbprophet import Prophet
-from itertools import zip_longest
 from flask import request, redirect
 from flask import render_template, request, session
-from werkzeug.utils import secure_filename
-from src import app
-from src.utils import get_historical_stock_price
+from src.utilities import MasterProphet
 
+
+@app.after_request
+def add_header(response):
+	response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+	response.headers['Cache-Control'] = 'public, max-age=0'
+	return response
 
 @app.route("/")
 @app.route("/home")
 def home():
-    ''' Renders the home page '''
-    session["date"] = datetime.now()
-    return render_template(
-        "index.html"
-    )
-
-@app.after_request
-def add_header(response):
-    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    response.headers['Cache-Control'] = 'public, max-age=0'
-    return response
+	''' Renders the home page '''
+	return render_template(
+		"index.html"
+	)
 
 @app.route("/predict", methods=["POST", "GET"])
 def predict():
-    stock = request.form['ticker']
-    df_whole = get_historical_stock_price(stock)
-
-    df = df_whole.filter(['Close'])
-
-    df['ds'] = df.index
-    df['y'] = np.log(df['Close'])
-    original_end = df['Close'][-1]
-
-    model = Prophet()
-    model.fit(df)
-
-    num_days = 10
-    future = model.make_future_dataframe(periods=num_days)
-    forecast = model.predict(future)
-
-    print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail())
-
-    df.set_index('ds', inplace=True)
-    forecast.set_index('ds', inplace=True)
-
-    viz_df = df.join(forecast[['yhat', 'yhat_lower','yhat_upper']], how = 'outer')
-    viz_df['yhat_scaled'] = np.exp(viz_df['yhat'])
-
-    close_data = viz_df.Close
-    forecasted_data = viz_df.yhat_scaled
-    date = future['ds']
-    forecast_start = forecasted_data[-num_days]
-
-    d = [date, close_data, forecasted_data]
-    export_data = zip_longest(*d, fillvalue = '')
-    database_path = os.path.join(str(os.getcwd()), "database", "numbers.csv")
-    with open(database_path, 'w', encoding="ISO-8859-1", newline='') as myfile:
-        wr = csv.writer(myfile)
-        wr.writerow(("Date", "Actual", "Forecasted"))
-        wr.writerows(export_data)
-    myfile.close()
-
-    return render_template(
-        "output.html",
-        original = round(original_end,2),
-        forecast = round(forecast_start,2),
-        stock_tinker = stock.upper()
-        )
+	ticker = request.form['ticker']
+	
+	master_prophet = MasterProphet(ticker)
+	forecast = master_prophet.forecast()
+	actual_forecast = round(forecast.yhat[0], 2)
+	lower_bound = round(forecast.yhat_lower[0], 2)
+	upper_bound = round(forecast.yhat_upper[0], 2)
+	
+	bound = round(((upper_bound - actual_forecast) + (actual_forecast - lower_bound) / 2), 2)
+		
+	summary = master_prophet.info["summary"]
+	country = master_prophet.info["country"]
+	sector = master_prophet.info["sector"]
+	website = master_prophet.info["website"]
+	min_date = master_prophet.info["min_date"]
+	max_date = master_prophet.info["max_date"]
+	forecast_date = master_prophet.forecast_date.date()
+	
+	return render_template(
+		"output.html",
+		ticker = ticker.upper(),
+		sector = sector,
+		country = country,
+		website = website,
+		summary = summary,
+		min_date = min_date,
+		max_date = max_date,
+		forecast_date = forecast_date,
+		forecast = actual_forecast,
+		bound = bound 
+		)
